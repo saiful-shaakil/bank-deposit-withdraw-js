7d3c61b3fe46087513e4566a6d163c1d1881a30e
si-shakil <sishakil850@gmail.com>
2022-02-18 16:00:22 +0600

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function getInput(fieldId) {
+    const amount = document.getElementById(fieldId);
+    const oldAmount = amount.innerText;
+    const prevAmount = parseFloat(oldAmount);
+};
 
+document.getElementById('deposit-submit').addEventListener('click', function () {
+    const inputDepositAmount = document.getElementById('deposit');
+
+})
+document.getElementById('withdraw-submit').addEventListener('click', function () {
+    const inputWithdrawAmount = document.getElementById('withdraw-input');
+
+})
